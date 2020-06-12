da415d337c288c044dc1ab40b486f3930bacee16
Aric Repp <aricmrepp@gmail.com>
2020-06-11 21:05:58 -0400

    pushing mvp

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,51 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import ContactForm from './components/ContactForm';
+import {
+  getByPlaceholderText,
+  getByLabelText,
+  getByText,
+  getByTestId,
+  queryByTestId,
+  // Tip: all queries are also exposed on an object
+  // called "queries" which you could import here as well
+  waitFor,
+  fireEvent,
+} from '@testing-library/dom'
+// adds special assertions like toHaveTextContent
+import '@testing-library/jest-dom/extend-expect'
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("checking first name", () => {
+  const { getByTestId } = render(<ContactForm />);
+
+  const fName = getByTestId(/firstname/i)
+
+  fireEvent.change(fName, {target: {value: 'Aric'}});
+  expect(fName.value).toBe('Aric');
+
+}); 
+
+test("checking last name", () => {
+  const { getByTestId } = render(<ContactForm />);
+
+  const lName = getByTestId(/lastname/i);
+
+  fireEvent.change(lName, {target: {value: 'Repp'}});
+  expect(lName.value).toBe('Repp');
+
+}); 
+
+test("checking email", () => {
+  const { getByTestId } = render(<ContactForm />);
+
+  const email = getByTestId(/email/i);
+
+  fireEvent.change(email, {target: {value: 'fakeemail@gmail.com'}});
+  expect(email.value).toBe('fakeemail@gmail.com');
+
+}); 
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -18,7 +18,8 @@ const ContactForm = () => {
           <input
             name="firstName"
             placeholder="Edd"
-            ref={register({ required: true, maxLength: 3 })}
+            data-testid="firstname"
+            ref={register({ required: true, maxLength: 10 })}
           />
           {errors.firstName && (
             <p>Looks like there was an error: {errors.firstName.type}</p>
@@ -30,6 +31,7 @@ const ContactForm = () => {
           <input
             name="lastName"
             placeholder="Burke"
+            data-testid="lastname"
             ref={register({ required: true })}
           />
           {errors.lastName && (
@@ -41,7 +43,7 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input name="email" ref={register({ required: true })} />
+          <input name="email" data-testid="email" ref={register({ required: true })} />
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
